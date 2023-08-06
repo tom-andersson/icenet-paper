bfab018beb76e3582acfa20e4495cdd518556cb9
Bryan <129260579+bryandunn614@users.noreply.github.com>
2023-08-06 22:52:52 +1000

    Update models.py

--- icenet/models.py
+++ icenet/models.py
@@ -7,9 +7,161 @@ import pandas as pd
 import xarray as xr
 import tensorflow as tf
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Conv2D, BatchNormalization, UpSampling2D, \
-    concatenate, MaxPooling2D, Input
+from tensorflow.keras.layers import Conv2D, BatchNormalization, UpSampling2D, concatenate, MaxPooling2D, Input
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import DepthwiseConv2D, Conv2D
+from tensorflow.keras.layers import Dropout, LeakyReLU, Add
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Reshape, GlobalMaxPooling2D, Activation, multiply
+from tensorflow.keras.layers import Lambda, Concatenate
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Permute
+
+
+
+def ResidualConv2D(filters, kernel_size, activation='relu', padding='same', kernel_initializer='he_normal'):
+    """
+    这个ResidualConv2D函数定义了一个残差块，其中包括快捷连接和两个自定义可分离卷积层。
+    快捷连接通过直接将输入连接到输出，提供了一种跳过一些层的机制，有助于缓解深度网络中的梯度消失问题。
+    """
+    def layer(input_tensor):
+       
+        shortcut = Conv2D(filters, (1, 1), padding=padding, kernel_initializer=kernel_initializer)(input_tensor)
+        x = CustomSeparableConv2D(filters, kernel_size, activation=None, padding=padding, kernel_initializer=kernel_initializer)(input_tensor)
+        x = LeakyReLU(alpha=0.1)(x)
+        x = CustomSeparableConv2D(filters, kernel_size, activation=None, padding=padding, kernel_initializer=kernel_initializer)(x)
+        x = LeakyReLU(alpha=0.1)(x)
+        x = Add()([shortcut, x])
+
+        return x
+    return layer
+
+
+class CustomSeparableConv2D(tf.keras.layers.Layer):
+    def __init__(self, filters, kernel_size, padding='SAME', kernel_initializer='he_normal', activation='relu', **kwargs):
+        super(CustomSeparableConv2D, self).__init__(**kwargs)
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.activation = tf.keras.activations.get(activation)
+        self.padding = padding.upper()
+        if kernel_initializer == 'he_normal':
+            self.kernel_initializer = tf.keras.initializers.HeNormal()
+        else:
+            self.kernel_initializer = tf.keras.initializers.GlorotUniform()
+
+    def build(self, input_shape):
+        input_channels = input_shape[-1]
+
+        # Depthwise convolution
+        self.depthwise_filter = self.add_weight(
+            shape=(self.kernel_size, self.kernel_size) + (input_channels, 1),
+            initializer=self.kernel_initializer,
+            trainable=True,
+            name='depthwise_filter'
+        )
+
+        # Pointwise convolution
+        self.pointwise_filter = self.add_weight(
+            shape=(1, 1, input_channels, self.filters),
+            initializer=self.kernel_initializer,
+            trainable=True,
+            name='pointwise_filter'
+        )
+
+    def call(self, inputs):
+        depthwise_output = tf.nn.depthwise_conv2d(
+            inputs,
+            self.depthwise_filter,
+            strides=(1, 1, 1, 1),
+            padding=self.padding
+        )
+
+        pointwise_output = tf.nn.conv2d(
+            depthwise_output,
+            self.pointwise_filter,
+            strides=(1, 1, 1, 1),
+            padding=self.padding
+        )
+
+        # pointwise_output = convolution_2d(depthwise_output,
+        # self.pointwise_filter, self.padding.lower())
+
+        output = self.activation(pointwise_output)
+        return output
+
+    def get_config(self):
+        config = super(CustomSeparableConv2D, self).get_config()
+        config.update({
+            'filters': self.filters,
+            'kernel_size': self.kernel_size,
+            'activation': tf.keras.activations.serialize(self.activation)
+        })
+        return config
+
+def channel_attention(input_feature, ratio=8):
+    """
+    该函数实现了通道注意力机制，这是一种能够使模型专注于输入特征的重要通道的技术。
+    它通过对输入特征执行全局平均池化和全局最大池化，并学习每个通道的权重来实现。
+    然后，这些权重与原始输入相乘，从而强调重要通道的贡献并减弱不重要通道的贡献。
+    """
+    channel_axis = 1 if tf.keras.backend.image_data_format() == "channels_first" else -1
+    channel = input_feature.shape[channel_axis]
+    shared_layer_one = Dense(channel // ratio,
+                             activation='relu',
+                             kernel_initializer='he_normal',
+                             use_bias=True,
+                             bias_initializer='zeros')
+    shared_layer_two = Dense(channel,
+                             kernel_initializer='he_normal',
+                             use_bias=True,
+                             bias_initializer='zeros')
+
+    avg_pool = GlobalAveragePooling2D()(input_feature)
+    avg_pool = Reshape((1, 1, channel))(avg_pool)
+    avg_pool = shared_layer_one(avg_pool)
+    avg_pool = shared_layer_two(avg_pool)
+
+    max_pool = GlobalMaxPooling2D()(input_feature)
+    max_pool = Reshape((1, 1, channel))(max_pool)
+    max_pool = shared_layer_one(max_pool)
+    max_pool = shared_layer_two(max_pool)
+
+    cbam_feature = Add()([avg_pool, max_pool])
+    cbam_feature = Activation('sigmoid')(cbam_feature)
+
+    return multiply([input_feature, cbam_feature])
+
+
+def spatial_attention(input_feature):
+    """
+    这个函数实现了一种称为空间注意力机制的技术，它使模型能够关注输入特征中的重要空间位置。
+    这是通过计算输入特征的平均值和最大值，然后将它们连接并通过一个卷积层来实现的。
+    最后，得到的空间注意力图与原始输入相乘，从而强调输入中重要位置的特征，并减弱不重要位置的特征。
+    """
+    kernel_size = 7
+
+    if tf.keras.backend.image_data_format() == "channels_first":
+        channel = input_feature.shape[1]
+        cbam_feature = Permute((2, 3, 1))(input_feature)
+    else:
+        channel = input_feature.shape[-1]
+        cbam_feature = input_feature
+
+    avg_pool = Lambda(lambda x: K.mean(x, axis=3, keepdims=True))(cbam_feature)
+    max_pool = Lambda(lambda x: K.max(x, axis=3, keepdims=True))(cbam_feature)
+    concat = Concatenate(axis=3)([avg_pool, max_pool])
+    cbam_feature = Conv2D(filters=1,
+                          kernel_size=kernel_size,
+                          strides=1,
+                          padding='same',
+                          activation='sigmoid',
+                          kernel_initializer='he_normal',
+                          use_bias=False)(concat)
+
+    if tf.keras.backend.image_data_format() == "channels_first":
+        cbam_feature = Permute((3, 1, 2))(cbam_feature)
+
+    return multiply([input_feature, cbam_feature])
+
 
 '''
 Defines the Python-based sea ice forecasting models, such as the IceNet architecture
@@ -19,7 +171,6 @@ and the linear trend extrapolation model.
 ### Custom layers:
 # --------------------------------------------------------------------
 
-
 @tf.keras.utils.register_keras_serializable()
 class TemperatureScale(tf.keras.layers.Layer):
     '''
@@ -27,13 +178,17 @@ class TemperatureScale(tf.keras.layers.Layer):
     as introduced in Guo 2017 (http://proceedings.mlr.press/v70/guo17a.html).
     '''
     def __init__(self, **kwargs):
+
         super(TemperatureScale, self).__init__()
+
         self.temp = tf.Variable(initial_value=1.0, trainable=False,
                                 dtype=tf.float32, name='temp')
 
     def call(self, inputs):
         ''' Divide the input logits by the T value. '''
+
         return tf.divide(inputs, self.temp)
+
 
     def get_config(self):
         ''' For saving and loading networks with this custom layer. '''
@@ -42,66 +197,78 @@ class TemperatureScale(tf.keras.layers.Layer):
 
 ### Network architectures:
 # --------------------------------------------------------------------
-
 def unet_batchnorm(input_shape, loss, weighted_metrics, learning_rate=1e-4, filter_size=3,
                    n_filters_factor=1, n_forecast_months=1, use_temp_scaling=False,
                    n_output_classes=3,
                    **kwargs):
+    """
+    这个函数定义了一个U-Net结构的卷积神经网络，具有注意力机制和批量归一化。
+    U-Net是一个全卷积网络，具有对称的编码器和解码器结构，适用于图像分割任务。
+    在此实现中，还添加了通道和空间注意力机制，以及可选的温度缩放层。
+    """
     inputs = Input(shape=input_shape)
 
-    conv1 = Conv2D(np.int(64*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
-    conv1 = Conv2D(np.int(64*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
+    conv1 = CustomSeparableConv2D(int(64 * n_filters_factor), filter_size, activation='relu', padding='same',
+                                  kernel_initializer='he_normal')(inputs)
+    conv1 = CustomSeparableConv2D(int(64 * n_filters_factor), filter_size, activation='relu', padding='same',
+                                  kernel_initializer='he_normal')(conv1)
+    conv1 = channel_attention(conv1)  
+    conv1 = spatial_attention(conv1)  
     bn1 = BatchNormalization(axis=-1)(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(bn1)
 
-    conv2 = Conv2D(np.int(128*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
-    conv2 = Conv2D(np.int(128*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
+    conv2 = ResidualConv2D(np.int64(128 * n_filters_factor), filter_size)(pool1)
+    conv2 = ResidualConv2D(np.int64(128 * n_filters_factor), filter_size)(conv2)
     bn2 = BatchNormalization(axis=-1)(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(bn2)
+    drop2 = Dropout(0.3)(bn2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(drop2)
 
-    conv3 = Conv2D(np.int(256*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
-    conv3 = Conv2D(np.int(256*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
+    conv3 = ResidualConv2D(np.int64(256 * n_filters_factor), filter_size)(pool2)
+    conv3 = ResidualConv2D(np.int64(256 * n_filters_factor), filter_size)(conv3)
     bn3 = BatchNormalization(axis=-1)(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(bn3)
+    drop3 = Dropout(0.3)(bn3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(drop3)
 
-    conv4 = Conv2D(np.int(256*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
-    conv4 = Conv2D(np.int(256*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
+    conv4 = CustomSeparableConv2D(np.int64(256*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+    conv4 = CustomSeparableConv2D(np.int64(256*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
     bn4 = BatchNormalization(axis=-1)(conv4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(bn4)
 
-    conv5 = Conv2D(np.int(512*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
-    conv5 = Conv2D(np.int(512*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
+    conv5 = CustomSeparableConv2D(np.int64(512*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+    conv5 = CustomSeparableConv2D(np.int64(512*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
     bn5 = BatchNormalization(axis=-1)(conv5)
 
-    up6 = Conv2D(np.int(256*n_filters_factor), 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2,2), interpolation='nearest')(bn5))
+    up6 = CustomSeparableConv2D(np.int64(256*n_filters_factor), 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2,2), interpolation='nearest')(bn5))
     merge6 = concatenate([bn4, up6], axis=3)
-    conv6 = Conv2D(np.int(256*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
-    conv6 = Conv2D(np.int(256*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
+    conv6 = CustomSeparableConv2D(np.int64(256*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
+    conv6 = CustomSeparableConv2D(np.int64(256*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
     bn6 = BatchNormalization(axis=-1)(conv6)
 
-    up7 = Conv2D(np.int(256*n_filters_factor), 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2,2), interpolation='nearest')(bn6))
+    up7 = CustomSeparableConv2D(np.int64(256*n_filters_factor), 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2,2), interpolation='nearest')(bn6))
     merge7 = concatenate([bn3,up7], axis=3)
-    conv7 = Conv2D(np.int(256*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
-    conv7 = Conv2D(np.int(256*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
+    conv7 = CustomSeparableConv2D(np.int64(256*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
+    conv7 = CustomSeparableConv2D(np.int64(256*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
     bn7 = BatchNormalization(axis=-1)(conv7)
 
-    up8 = Conv2D(np.int(128*n_filters_factor), 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2,2), interpolation='nearest')(bn7))
+    up8 = CustomSeparableConv2D(np.int64(128*n_filters_factor), 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2,2), interpolation='nearest')(bn7))
     merge8 = concatenate([bn2,up8], axis=3)
-    conv8 = Conv2D(np.int(128*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
-    conv8 = Conv2D(np.int(128*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
+    conv8 = CustomSeparableConv2D(np.int64(128*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
+    conv8 = CustomSeparableConv2D(np.int64(128*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
     bn8 = BatchNormalization(axis=-1)(conv8)
 
-    up9 = Conv2D(np.int(64*n_filters_factor), 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2,2), interpolation='nearest')(bn8))
+    up9 = CustomSeparableConv2D(np.int64(64*n_filters_factor), 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2,2), interpolation='nearest')(bn8))
     merge9 = concatenate([conv1,up9], axis=3)
-    conv9 = Conv2D(np.int(64*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
-    conv9 = Conv2D(np.int(64*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
-    conv9 = Conv2D(np.int(64*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+    conv9 = CustomSeparableConv2D(np.int64(64*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
+    conv9 = CustomSeparableConv2D(np.int64(64*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+    conv9 = CustomSeparableConv2D(np.int64(64*n_filters_factor), filter_size, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
 
-    final_layer_logits = [(Conv2D(n_output_classes, 1, activation='linear')(conv9)) for i in range(n_forecast_months)]
+    conv9 = channel_attention(conv9)  
+    conv9 = spatial_attention(conv9)  
+
+    final_layer_logits = [(CustomSeparableConv2D(n_output_classes, 1, activation='linear')(conv9)) for i in range(n_forecast_months)]
     final_layer_logits = tf.stack(final_layer_logits, axis=-1)
 
     if use_temp_scaling:
-        # Temperature scaling of the logits
         final_layer_logits_scaled = TemperatureScale()(final_layer_logits)
         final_layer = tf.nn.softmax(final_layer_logits_scaled, axis=-2)
     else:
@@ -149,7 +316,6 @@ def linear_trend_forecast(forecast_month, n_linear_years='all', da=None, dataset
     valid_dates = [pd.Timestamp(date) for date in da.time.values]
 
     input_dates = [forecast_month - pd.DateOffset(years=1+lag) for lag in range(n_linear_years)]
-    input_dates
 
     # Do not use missing months in the linear trend projection
     input_dates = [date for date in input_dates if date not in config.missing_dates]
@@ -185,3 +351,11 @@ def linear_trend_forecast(forecast_month, n_linear_years='all', da=None, dataset
     sie = np.sum(output_map > 0.15) * 25**2
 
     return output_map, sie
+
+def loss(y_true, y_pred):
+    y_true = tf.cast(y_true, tf.float32)
+    return tf.reduce_mean(tf.square(y_true - y_pred))
+
+
+def weighted_metrics(y_true, y_pred):
+    return tf.reduce_mean(tf.abs(y_true - y_pred))
